1be9299acecc8447919b1bbd916ab40f8412696b
Tadi23 <89647605+Tadi23@users.noreply.github.com>
2025-07-09 16:13:04 +0200

    expo build
    the expo build of the app

--- UJ-Bus-App/App.tsx
+++ UJ-Bus-App/App.tsx
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import Home from './screens/Home';
 import Schedule from './screens/Schedule';
 import Rating from './screens/Rating';
